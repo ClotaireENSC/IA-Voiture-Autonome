41e7b8194022bb83b8835f2d0e5034bb5c154bad
Clotaire <cmardon001@ensc.fr>
2024-01-22 11:58:25 +0100

    Initial commit

---
+++ Assets/Scripts/TestCubeMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class TestCubeMovement : MonoBehaviour
+{
+public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("z"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
+        }
+        else if (Input.GetKey("z") && !Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey("q") && !Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("q"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+
+
+    }
+}
