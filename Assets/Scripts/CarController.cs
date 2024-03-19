ed2cfad86772c86e32b38ffef27bc07e08cca907
ClotaireENSC <cmardon001@ensc.fr>
2024-03-19 16:27:01 +0100

    Commentaires

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,18 +2,29 @@ using System;
 using TMPro;
 using UnityEngine;
 
+/*
+CarController est une classe attachée à une voiture pour la contrôler
+
+Globalement elle contient toutes les informations relatives au mouvement (Vitesse actuelle, Boutons appuyés, état de collision, ...)
+et un réseau de neurones de type "NeuralNetwork"
+*/
+
 public class CarController : MonoBehaviour
 {
     System.Random rnd = new System.Random();
 
+    // Parametres de la voiture
     public float currentSpeed;
     public float maxSpeed = 50;
+    public bool Collision = false;
+
+    // "Boutons" actionnés par la voiture
     private int Accelerate = 0;
     private int Decelerate = 0;
     public int TurnRight = 0;
     public int TurnLeft = 0;
-    public bool Collision = false;
 
+    // Initialisation des rayons entourant la voiture pour l'IA
     private Ray[] ray;
     private float rayLength = 10f;
 
@@ -21,13 +32,13 @@ public class CarController : MonoBehaviour
 
     public int score = 0;
 
-    public TextMeshPro scoreText;
-
     private void Start()
     {
         InitializeCar();
     }
 
+
+    // Initialisation de la voiture (Initialisation des rayons)
     private void InitializeCar()
     {
         currentSpeed = 0;
@@ -37,14 +48,15 @@ public class CarController : MonoBehaviour
         {
             ray[i] = new Ray();
         }
-
-        scoreText = GetComponentInChildren<TextMeshPro>();
     }
 
+
+    // Fonction éxecutée dès que la voiture entre en collision avec un élément
     private void OnCollisionEnter(Collision collision)
     {
         if (!Collision)
         {
+            // Si la voiture collisionne un mur, elle s'arrête
             if (collision.gameObject.name == "Guard" ||
                 collision.gameObject.name == "Extra Guard" ||
                 collision.gameObject.name == "Guard_2" ||
@@ -56,41 +68,41 @@ public class CarController : MonoBehaviour
         }
     }
 
+
+    // Fonction éxecutée quand la voiture croise un objet
     private void OnTriggerEnter(Collider other)
     {
+        //Si la voiture croise un checkpoint dans le bon sens elle augmente son score, sinon l'inverse
         if (other.gameObject.CompareTag("Checkpoint"))
         {
-            if (IsSameDirection(transform.forward, other.transform.forward))
-            {
-                score++;
-            }
-            else
-            {
-                score--;
-            }
+            score += IsSameDirection(transform.forward, other.transform.forward) ? 1 : -1;
         }
     }
 
+
+    // Fonction qui regarde si 2 directions sont dans le même sens (pour les checkpoints notamment)
     private bool IsSameDirection(Vector3 direction1, Vector3 direction2)
     {
         return Vector3.Dot(direction1.normalized, direction2.normalized) > 0.9f;
     }
 
 
+    // Fonction éxecutée à chaque frame de la simulation
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
-            CheckRay();
-
         //GetUserKeys();
+
+        // Récupération des choix de l'IA
         GetAiKeys();
 
-        ShowText();
 
+        // Tant que la voiture n'est pas collisionée, elle bouge
         if (!Collision)
         {
             Move();
             Rotate();
+
+            // Chance d'appliquer une rotation aléatoire (pour éviter le surapprentissage)
             if (rnd.NextDouble() < 0.001)
             {
                 RndRotate();
@@ -98,47 +110,42 @@ public class CarController : MonoBehaviour
         }
     }
 
-    public void CheckRay()
-    {
-        int layerMask = 1 << LayerMask.NameToLayer("Checkpoints");
-        layerMask = layerMask | (1 << LayerMask.NameToLayer("Voitures"));
 
-        RaycastHit hitInfo;
-
-        Physics.Raycast(ray[0], out hitInfo, rayLength + 40, ~layerMask);
-        Debug.DrawRay(ray[0].origin, ray[0].direction * hitInfo.distance, Color.blue);
-
-        for (int i = 1; i < ray.Length; i++)
-        {
-            Physics.Raycast(ray[i], out hitInfo, rayLength, ~layerMask);
-            Debug.DrawRay(ray[i].origin, ray[i].direction * hitInfo.distance, Color.blue);
-        }
-    }
-
+    // Modification de la vitesse actuelle selon les "Touches appuyées" par l'IA
     private void Move()
     {
-        if (Accelerate > 0.1)
-        {
-            currentSpeed = Mathf.Lerp(currentSpeed, maxSpeed, Time.deltaTime * 0.1f);
-        }
-        else if (Decelerate > 0.1)
-        {
-            currentSpeed = Mathf.Lerp(currentSpeed, 0, Time.deltaTime * 2f);
-        }
-        else
-        {
-            currentSpeed = Mathf.Lerp(currentSpeed, 0, Time.deltaTime * 1f);
-        }
+        currentSpeed = (Accelerate > 0.1) ? Mathf.Lerp(currentSpeed, maxSpeed, Time.deltaTime * 0.1f) :
+               (Decelerate > 0.1) ? Mathf.Lerp(currentSpeed, 0, Time.deltaTime * 2f) :
+               Mathf.Lerp(currentSpeed, 0, Time.deltaTime * 1f);
 
-        if (currentSpeed < 0.1f && Accelerate < 0.1)
-        {
-            currentSpeed = 0f;
-        }
+        currentSpeed = (currentSpeed < 0.1f && Accelerate < 0.1) ? 0f : currentSpeed;
+
+        // Version plus lisible
+
+        // if (Accelerate > 0.1)
+        // {
+        //     currentSpeed = Mathf.Lerp(currentSpeed, maxSpeed, Time.deltaTime * 0.1f);
+        // }
+        // else if (Decelerate > 0.1)
+        // {
+        //     currentSpeed = Mathf.Lerp(currentSpeed, 0, Time.deltaTime * 2f);
+        // }
+        // else
+        // {
+        //     currentSpeed = Mathf.Lerp(currentSpeed, 0, Time.deltaTime * 1f);
+        // }
+
+        // if (currentSpeed < 0.1f && Accelerate < 0.1)
+        // {
+        //     currentSpeed = 0f;
+        // }
 
         transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
         RayFollow();
     }
 
+
+    // Suivi des rayons sur la voiture
     private void RayFollow()
     {
         float rayHeight = 0.15f; // Hauteur du rayon par rapport � la position du GameObject
@@ -164,34 +171,49 @@ public class CarController : MonoBehaviour
         ray[4].direction = transform.forward - transform.right;
     }
 
+
+    // Modification de la rotation de la voiture en fonction des "touches appuyées" par l'IA
     private void Rotate()
     {
         float angle = 0;
-        if (TurnRight > 0.1)
-        {
-            angle += 90;
-        }
-        if (TurnLeft > 0.1)
-        {
-            angle -= 90;
-        }
 
-        if (currentSpeed < 0)
-        {
-            angle = -angle;
-        }
+        angle += (TurnRight > 0.1) ? 90 : (TurnLeft > 0.1) ? -90 : 0;
+
+        angle = (currentSpeed < 0) ? -angle : angle;
 
         if (currentSpeed != 0)
         {
-            transform.Rotate(0, Time.deltaTime * angle * 20f / (Mathf.Abs(currentSpeed) + 10f), 0);
+            float rotationSpeed = Time.deltaTime * angle * 20f / (Mathf.Abs(currentSpeed) + 10f);
+            transform.Rotate(0, rotationSpeed, 0);
         }
+
+
+        // Version simplifiée
+
+        // if (TurnRight > 0.1)
+        // {
+        //     angle += 90;
+        // }
+        // if (TurnLeft > 0.1)
+        // {
+        //     angle -= 90;
+        // }
+
+        // if (currentSpeed < 0)
+        // {
+        //     angle = -angle;
+        // }
     }
 
+
+    // Applique une rotation aléatoire à la voiture
     private void RndRotate()
     {
         transform.Rotate(0, rnd.Next(0, 1), 0);
     }
 
+
+    // Fonction Utilisée pour les tests de circuit (L'utilisateur contrôle)
     private void GetUserKeys()
     {
         Accelerate = 0;
@@ -218,41 +240,39 @@ public class CarController : MonoBehaviour
         }
     }
 
+
+    // Récupération des actions de l'IA
+    // RDN : Réseau de Neurones
     private void GetAiKeys()
     {
         int nbInputs = NeuralNetwork.LayersLengths[0];
-
         double[] inputs = new double[nbInputs];
+
+        // Premier input du RDN : Vitesse actuelle normalisée
         inputs[0] = currentSpeed / maxSpeed;
 
+
+        // Initialisation des calculs de rayons
         int layerMask = 1 << LayerMask.NameToLayer("Checkpoints");
         layerMask = layerMask | (1 << LayerMask.NameToLayer("Voitures"));
 
         RaycastHit hitInfo;
 
+        // Deuxième input du RDN : Distance devant à 40m normalisée
         inputs[1] = Physics.Raycast(ray[0], out hitInfo, (rayLength + 40), ~layerMask) ? hitInfo.distance / (rayLength + 40) : (rayLength + 40);
 
+        // Les 4 derniers inputs sont les 4 autres rayons autour 
         for (int i = 2; i < nbInputs; i++)
         {
             inputs[i] = Physics.Raycast(ray[i - 1], out hitInfo, rayLength, ~layerMask) ? hitInfo.distance / rayLength : rayLength;
         }
 
+
+        // Calcul des sorties en fonction des entrées
         double[] outputs = NeuralNetwork.Brain(inputs);
         Accelerate = Convert.ToInt32(outputs[0]);
         Decelerate = Convert.ToInt32(outputs[1]);
         TurnRight = Convert.ToInt32(outputs[2]);
         TurnLeft = Convert.ToInt32(outputs[3]);
-    }
-
-    public void ShowText()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            scoreText.text = score.ToString("F0");
-        }
-        else
-        {
-            scoreText.text = "";
-        }
     }
 }
